78f8997978d4e41067f53c37670ad90619168abd
LebedevMar9n <lebedevmar9n1991@gmail.com>
2023-01-30 16:42:02 +0200

    added console-logger (implenting  SingleTone pattern(one instance)) and added command.executor (implementing template pattern)

--- src/out/console-logger/console-logger.ts
+++ src/out/console-logger/console-logger.ts
@@ -1,4 +1,4 @@
-import { IStreamLogger } from './../../handlers/stream-logger.interface';
+import { IStreamLogger } from '../../core/handlers/stream-logger.interface';
 
 export class ConsoleLogger implements IStreamLogger {
     // implenting  SingleTone pattern(one instance)
