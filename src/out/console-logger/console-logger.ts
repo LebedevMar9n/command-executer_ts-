5161d9d14fd2860231605b35fde4bee7e871a429
LebedevMar9n <lebedevmar9n1991@gmail.com>
2023-01-30 16:11:11 +0200

    added console-logger (implenting  SingleTone pattern(one instance))

---
+++ src/out/console-logger/console-logger.ts
@@ -0,0 +1,23 @@
+import { IStreamLogger } from './../../handlers/stream-logger.interface';
+
+export class ConsoleLogger implements IStreamLogger {
+    // implenting  SingleTone pattern(one instance)
+    private static logger: ConsoleLogger;
+    public static getInstance() {
+        if (!ConsoleLogger.logger) {
+            ConsoleLogger.logger = new ConsoleLogger();
+        }
+        return ConsoleLogger.logger;
+    }
+
+    log(...args: any[]): void {
+        console.log(...args);
+    }
+    error(...args: any[]): void {
+        console.log(...args);
+    }
+    end(): void {
+        console.log('Done');
+    }
+
+}
