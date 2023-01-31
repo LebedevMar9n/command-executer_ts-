acfd1e6f2d8ff24bffecc24243ccc0977aa7d803
LebedevMar9n <lebedevmar9n1991@gmail.com>
2023-01-31 17:00:28 +0200

    added ffmpeg executor

---
+++ src/commands/ffmpeg.executor.ts
@@ -0,0 +1,41 @@
+import { FfmpegBuilder } from './ffmpeg/ffmpeg.builder';
+import { IFfmpegInput, ICommandExecFfmpeg } from './ffmpeg/ffmpeg.types';
+import { PromptService } from './../core/prompt/prompt.service';
+import { FileService } from './../core/files/file.service';
+import { IStreamLogger } from '../core/handlers/stream-logger.interface';
+import { CommandExecutor } from '../core/executor/command.executor';
+import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
+import { StreamHandler } from '../core/handlers/stream-handlers';
+
+export class FfmpegExecutor extends CommandExecutor<IFfmpegInput>{
+    private fileService: FileService = new FileService();
+    private pronptService: PromptService = new PromptService();
+
+    constructor(logger: IStreamLogger) {
+        super(logger);
+    }
+
+    protected async prompt(): Promise<IFfmpegInput> {
+        const width = await this.pronptService.input<number>('Enter width', 'number');
+        const height = await this.pronptService.input<number>('Enter height', 'number');
+        const path = await this.pronptService.input<string>('Enter file path', 'input');
+        const name = await this.pronptService.input<string>('Enter file heigth', 'input');
+        return { width, height, path, name };
+    }
+    protected build({ width, height, path, name }: IFfmpegInput): ICommandExecFfmpeg {
+        const output = this.fileService.getFilePath(path, name, 'mp4');
+        const args = (new FfmpegBuilder)
+            .input(path)
+            .setVideoSize(width, height)
+            .output(output);
+        return { command: 'ffmpeg', args, output };
+    }
+    protected spawn({ output, command, args }: ICommandExecFfmpeg): ChildProcessWithoutNullStreams {
+        this.fileService.deleteFileIfExist(output);
+        return spawn(command, args);
+    }
+    protected processStream(stream: ChildProcessWithoutNullStreams, logger: IStreamLogger): void {
+        const handler = new StreamHandler(logger);
+        handler.processOutput(stream);
+    }
+}
